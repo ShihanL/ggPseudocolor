8d49c67edbe80d3aa4e59b2bee0517de3850e146
shihanl1 <shihan.li@unimelb.edu.au>
2025-05-13 15:19:41 +1000

    nn_neighbour version bug

--- R/ggpseudocolor.R
+++ R/ggpseudocolor.R
@@ -44,10 +44,10 @@ StatNN <- ggplot2::ggproto("StatNN", ggplot2::Stat,
                                     compute_group = function(data, scales, n) {
                                         print(data.frame(as.numeric(data$x),
                                                          as.numeric(data$y)))
-                                        neighbour_dist = FNN::get.knn(data.frame(as.numeric(data$x),
-                                                                                 as.numeric(data$y)),
-                                                                      k = n)
-                                        data$density <- rowMeans(neighbour_dist$nn.dist)
+                                        #neighbour_dist = FNN::get.knn(data.frame(as.numeric(data$x),
+                                        #                                         as.numeric(data$y)),
+                                        #                              k = n)
+                                        data$density <- rowMeans(data$x)
                                         data
                                     }
 )
